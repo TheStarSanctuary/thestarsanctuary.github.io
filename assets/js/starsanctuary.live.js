972ffa295dfa37ec9297a2509ca1f896b0ae6568
DreadJaw <thedreadjaw@gmail.com>
2020-01-02 22:56:58 -0500

    Full width mod activate

--- assets/js/starsanctuary.live.js
+++ assets/js/starsanctuary.live.js
@@ -7,7 +7,7 @@ if(streamerList.indexOf(streamer) !== -1) {
         allowfullscreen: true,
         layout: 'video-with-chat',
         width: '100%',
-        height: 500,
+        height: '100vh',
         theme: 'dark',
         autoplay: true
     })
