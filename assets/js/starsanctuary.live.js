7d7e63ffd67bed1e1129554022f8829ac17989ab
DreadJaw <thedreadjaw@gmail.com>
2020-01-02 22:28:59 -0500

    Added view live stream

---
+++ assets/js/starsanctuary.live.js
@@ -0,0 +1,14 @@
+var streamSearch = new URLSearchParams(window.location.search);
+var streamer = streamSearch.get('s');
+
+if(streamerList.indexOf(streamer) !== -1) {
+    new Twitch.Embed('streamContainer', {
+        channel: streamer,
+        allowfullscreen: true,
+        layout: 'video-with-chat',
+        theme: 'dark',
+        autoplay: true
+    })
+} else {
+    window.location.href = window.location.origin;
+}
---
+++ live.md
@@ -0,0 +1,4 @@
+---
+layout: livestream
+theme: core
+---
