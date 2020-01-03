af13bfa03f5dc36d505db732f899fc6d35d95a48
DreadJaw <thedreadjaw@gmail.com>
2020-01-03 02:06:10 -0500

    Updated for multitwitch

--- assets/js/starsanctuary.live.js
+++ assets/js/starsanctuary.live.js
@@ -1,16 +1,50 @@
 var streamSearch = new URLSearchParams(window.location.search);
 var streamer = streamSearch.get('s');
 
-if(streamerList.indexOf(streamer) !== -1) {
-    new Twitch.Embed('streamContainer', {
-        channel: streamer,
-        allowfullscreen: true,
-        layout: 'video-with-chat',
-        width: '100%',
-        height: '100%',
-        theme: 'dark',
-        autoplay: true
-    })
+function createColumns(streamers, columns) {
+    for(var i = 0; i < streamers; i++) {
+        $('#streamContainer').append(`<div id="stream${i}" class="col-md-${columns}"></div>`);
+    }
+}
+
+if(streamer.indexOf('+') !== -1) {
+    var streamers = streamer.split('+');
+    var valid = [];
+
+    for(var i = 0; i < streamers.length; i++) {
+        if(streamerList.indexOf(streamers[i]) !== -1) {
+            valid.push(streamers[i]);
+        }
+    }
+
+    var colCount = parseInt(12 / valid.length);
+    
+    createColumns(valid.length, colCount);
+    
+    for(var i = 0; i < valid.length; i++) {
+        new Twitch.Embed(`stream${i}`, {
+            channel: valid[i],
+            allowfullscreen: true,
+            layout: 'video-with-chat',
+            width: '100%',
+            height: '100%',
+            theme: 'dark',
+            autoplay: true
+        })
+    }
 } else {
-    window.location.href = window.location.origin;
+    $('#streamContainer').append('<div id="soloStream" class="col-md-12"></div>');
+    if(streamerList.indexOf(streamer) !== -1) {
+        new Twitch.Embed('soloStream', {
+            channel: streamer,
+            allowfullscreen: true,
+            layout: 'video-with-chat',
+            width: '100%',
+            height: '100%',
+            theme: 'dark',
+            autoplay: true
+        })
+    } else {
+        window.location.href = window.location.origin;
+    }
 }
